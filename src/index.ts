8363de54140fba93ee78a1d704378625cb28dc1f
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2022-12-04 18:01:17 -0500

    convert switch statement to dictionary

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,17 @@ audioPlayer.on('error', (error) => {
   console.log('Error with audio player:', error);
 });
 
+const discordUserAnnouncementDictionary: { [key: string]: string } = {
+  'kyhole#3631': 'shutUpKyle.mp3',
+  'robborg#4693': 'RobbieHasArrived.mp3',
+  'Jenkinz94#4030': 'NickHasArrived.mp3',
+  'mr.barron#9498': 'AlexHasArrived.mp3',
+  'Snapps#5034': 'SAMMMMM.mp3',
+  'bryborg#3434': 'thebryansong.mp3',
+  'Dru#7852': 'Dr_Dru_v2.mp3',
+  'lKoNFlicTl#3922': 'This_is_Patrick.mp3'
+};
+
 // Event triggered when a user changes voice state - e.g. joins/leaves a channel, mutes/unmutes, etc.
 client.on('voiceStateUpdate', async (oldState, newState) => {
   const introsEnabled = true;
@@ -77,33 +88,16 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
       const username = newState?.member?.user.tag as string;
 
       // Play a clip based on the username
-      switch (username) {
-        case 'kyhole#3631':
-          await playClip('shutUpKyle.mp3', channel, audioPlayer);
-          break;
-        case 'robborg#4693':
-          await playClip('RobbieHasArrived.mp3', channel, audioPlayer);
-          break;
-        case 'Jenkinz94#4030':
-          await playClip('NickHasArrived.mp3', channel, audioPlayer);
-          break;
-        case 'mr.barron#9498':
-          await playClip('AlexHasArrived.mp3', channel, audioPlayer);
-          break;
-        case 'Snapps#5034':
-          await playClip('SAMMMMM.mp3', channel, audioPlayer);
-          break;
-        case 'bryborg#3434':
-          await playClip('thebryansong.mp3', channel, audioPlayer);
-          break;
-        case 'Dru#7852':
-          await playClip('Dr_Dru_v2.mp3', channel, audioPlayer);
-          break;
-        case 'lKoNFlicTl#3922':
-          await playClip('This_is_Patrick.mp3', channel, audioPlayer);
-          break;
-        default:
+      if (discordUserAnnouncementDictionary[username]) {
+        playClip(
+          discordUserAnnouncementDictionary[username],
+          channel,
+          audioPlayer
+        );
+      } else {
+        if (username !== 'Big Announcer Dude#0867') {
           console.log('Unhandled user joined a voice channel.');
+        }
       }
     }
     // User (not a bot) exits channel with users still in it
