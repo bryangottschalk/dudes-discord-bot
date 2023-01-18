afc756859f5b0282e0641fbab5c3b13d7971ada9
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2023-01-18 18:06:30 -0500

    Add more clip options (#42)

--- src/constants.ts
+++ src/constants.ts
@@ -10,4 +10,32 @@ export const GUILD_ID: string = process.env.GUILD_ID || '';
 export const LEAGUE_OF_LEGENDS = 'League of Legends';
 
 // Clip options for playRandomClipFromList to handle
-export const KILL_CLIP_OPTIONS: string[] = ['PUNCH.mp3', 'shotgun-firing.mp3', 'bye_im_paul.mp3'];
+export const TEAMMATE_KILL_CLIP_OPTIONS: string[] = [
+  'PUNCH.mp3',
+  'shotgun-firing.mp3',
+  'bye_im_paul.mp3',
+  'weo_BOOM.mp3',
+  'who_gettin_money_this_brain.mp3'
+];
+
+export const TEAMMATE_DIED_CLIP_OPTIONS: string[] = [
+  'bugsplat2.mp3',
+  'this_guy_stinks.mp3',
+  'spongebob_steel_sting.mp3'
+];
+
+export const GAME_START_CLIP_OPTIONS: string[] = [
+  'halo_slayer.mp3',
+  'halo_assault.mp3',
+  'halo_king_of_the_hill.mp3',
+  'halo_capture_the_flag.mp3',
+  'halo_oddball.mp3'
+];
+
+export const GAME_WON_CLIP_OPTIONS: string[] = ['halo_victory_grunt.mp3', 'sweet_victory.mp3'];
+
+export const GAME_LOST_CLIP_OPTIONS: string[] = [
+  'loser_spongebob.mp3',
+  'smallest_violin_remix.mp3',
+  'mm_whatcha_say.mp3'
+];
